155bcd32bfe89fb1c8af1f5aa28c705f9d4e7bf6
yilingding <yilingding@email.arizona.edu>
2016-10-07 15:35:08 -0700

    Partner 1 committing implementation of MyList

--- src/MyList.java
+++ src/MyList.java
@@ -8,45 +8,40 @@
  */
 public class MyList {
 
-  String[] array;
+	private class Node {
+		private String data;
+		private Node next;
+	}
 
-  public MyList() {
-    this.array = new String[0];
-  }
-  
-  /**
-   * Adds the element as the first element of the list.
-   * You don't need to implement this fully, just have something that is a start
-   * using the appropriate structure.
-   */
-  public void addFirst(String element) {
-    // Very bad implementation, make a new array with one more element than
-    // the current array, add the element at index 0 and then copy over the
-    // previous elements. The adjust this.array to point at the new array.
-    String[] newArray = new String[this.array.length + 1];
-    newArray[0] = element;
-    for (int i = 0; i < this.array.length; i++) {
-      newArray[i + 1] = this.array[i];
-    }
-    this.array = newArray;
-  }
+	private Node head;
 
-  /**
-   * Removes the first element of the list.
-   * You don't need to implement this fully, must have something that is a start
-   * using the appropriate structure.
-   */
-  public void removeFirst() {
-    // Very bad implementation, make a new array with one less element than
-    // copy over the elements from the current array starting at index 1.
-    // Then adjust this.array to point at the new array.
-    if (this.array.length > 0) {
-      String[] newArray = new String[this.array.length - 1];
-      for (int i = 1; i < this.array.length; i++) {
-        newArray[i - 1] = this.array[i];
-      }
-      this.array = newArray;
-    }
+	public MyList() {
+		this.head = null;
+	}
 
-  }
+	/**
+	 * Adds the element as the first element of the list.
+	 * You don't need to implement this fully, just have something that is a start
+	 * using the appropriate structure.
+	 */
+	public void addFirst(String element) {
+		// Make a new node, with the element, then adjust head.
+		Node newHead = new Node();
+		newHead.data = element;
+		newHead.next = this.head;
+		this.head = newHead;
+	}
+
+	/**
+	 * Removes the first element of the list.
+	 * You don't need to implement this fully, must have something that is a start
+	 * using the appropriate structure.
+	 */
+	public void removeFirst() {
+		// If the head isn't null, then adjust head to point at its next.
+		if (this.head != null) {
+			this.head = this.head.next;
+		}
+	}
+
 }
